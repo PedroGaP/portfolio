ac38a2165b58219c78e3394ff546859ef2774a78
Jaen <jaennova9@gmail.com>
2024-12-30 17:11:56 -0600

    refactor: update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,28 +1,33 @@
+<script>
+  import personalData from "../data/personal.json";
+</script>
+
 <footer class="bg-white dark:bg-gray-900 py-8 mt-20">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="text-center">
       <p class="text-gray-600 dark:text-gray-400">
-        © {new Date().getFullYear()} Jaen Nova. All rights reserved.
+        © {new Date().getFullYear()}
+        {personalData.name}. Todos los derechos reservados.
       </p>
       <div class="mt-4 flex justify-center space-x-6">
         <a
-          href="https://github.com/jaennova"
+          href={personalData.contact.github.link}
           target="_blank"
           rel="noopener noreferrer"
           class="text-gray-600 dark:text-gray-400 hover:text-android-light dark:hover:text-android-light"
         >
-          GitHub
+          {personalData.contact.github.title}
         </a>
         <a
-          href="https://linkedin.com/in/jaennova"
+          href={personalData.contact.linkedin.link}
           target="_blank"
           rel="noopener noreferrer"
           class="text-gray-600 dark:text-gray-400 hover:text-android-light dark:hover:text-android-light"
         >
-          LinkedIn
+          {personalData.contact.linkedin.title}
         </a>
         <a
-          href="mailto:jaennova@outlook.com"
+          href="mailto:{personalData.contact.email}"
           class="text-gray-600 dark:text-gray-400 hover:text-android-light dark:hover:text-android-light"
         >
           Email
